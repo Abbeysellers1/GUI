890b8a9a9cbd2c2652f7715e0164f62f3611a708
Abbey <abbey_sellers1@baylor.edu>
2022-10-05 22:44:04 -0500

    updates 10.5

--- testscores.py
+++ testscores.py
@@ -15,7 +15,7 @@ class MyGUI:
         self.main_window.title('Test Scores')
 
 
-        self.avg_var=tkinter.StringVar()
+        #self.avg_var=tkinter.StringVar()
         
 
 
@@ -23,7 +23,7 @@ class MyGUI:
         self.test2frame = tkinter.Frame(self.main_window)
         self.test3frame = tkinter.Frame(self.main_window)
         self.averagelabel=tkinter.Frame(self.main_window)
-        
+        self.bottomframe=tkinter.Frame(self.main_window)
         
         self.prompt_test1=tkinter.Label(self.test1frame, text='Enter the score for test 1:')
         self.prompt_test2=tkinter.Label(self.test2frame, text='Enter the score for test 2:')
@@ -35,47 +35,55 @@ class MyGUI:
         self.test3_entry=tkinter.Entry(self.test3frame, width=3)
 
         self.prompt_test1.pack(side='left')
-        self.test1_entry.pack(side='right')
+        self.test1_entry.pack(side='left')
+
         self.prompt_test2.pack(side='left')
-        self.test2_entry.pack(side='right')
+        self.test2_entry.pack(side='left')
+
         self.prompt_test3.pack(side='left')
-        self.test3_entry.pack(side='right')
+        self.test3_entry.pack(side='left')
 
+        self.test1frame.pack()
+        self.test2frame.pack()
+        self.test3frame.pack()
 
-       # self.avg_label= tkinter.Label(self.averagelabel, textvariable=self.avg_var)
+ 
 
-      #  self.avg_label.pack(side='left')
-     
-      #  self.averagelabel=tkinter.Label(self.averagelabel, text='Average')
-      #  self.averagelabel.pack(side='left')
+        self.avg_label= tkinter.Label(self.averagelabel, text= 'Average')
+
+        self.avg_label.pack(side='left')
+
+        self.calculation= tkinter.StringVar()
+        self.averageanswer=tkinter.Label(self.averagelabel, textvariable=self.calculation)
+        self.averageanswer.pack(side='left')
+
 
         
-        self.calc_button=tkinter.Button(self.main_window, text='Average', command= self.convert)
+        self.calc_button=tkinter.Button(self.bottomframe, text='Average', command= self.calculate)
 
         #destroy will close the window
-        self.quitbutton=tkinter.Button(self.main_window, text='Quit', command= self.main_window.destroy)
+        self.quitbutton=tkinter.Button(self.bottomframe, text='Quit', command= self.main_window.destroy)
 
       
         self.calc_button.pack(side='left')
         self.quitbutton.pack(side='right')
 
+        self.averagelabel.pack()
+        self.bottomframe.pack()
+
 
         tkinter.mainloop()
 
 
-    def convert(self):
-        total= int(self.test1_entry.get())+int(self.test2_entry.get())+int(self.test3_entry.get())
-        average=statistics.mean(total)
-
-        tkinter.messagebox.showinfo('Average ', str(average))
-        tkinter.messagebox.showinfo()
-
-        self.avg_var.set()
+    def calculate(self):
+        total= float(self.test1_entry.get())+float(self.test2_entry.get())+float(self.test3_entry.get())
+        average=total/3
+        self.calculation.set(average)
 
 myGUI = MyGUI()
 
 #this will not be on the window because it is not in the loop, once you kill the window, it will show up
-print('Hi there!')
+
 
 
 
